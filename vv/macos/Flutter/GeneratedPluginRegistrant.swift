2e943d19e5c9b1343ce7be5abc9219779913a62e
Menna <moui48973@gmail.com>
2024-04-08 04:07:38 +0200

    done
    add package video

--- vv/macos/Flutter/GeneratedPluginRegistrant.swift
+++ vv/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -14,6 +14,7 @@ import just_audio
 import path_provider_foundation
 import record_darwin
 import sqflite
+import video_player_avfoundation
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   AudioSessionPlugin.register(with: registry.registrar(forPlugin: "AudioSessionPlugin"))
@@ -25,4 +26,5 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   RecordPlugin.register(with: registry.registrar(forPlugin: "RecordPlugin"))
   SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
+  FVPVideoPlayerPlugin.register(with: registry.registrar(forPlugin: "FVPVideoPlayerPlugin"))
 }
