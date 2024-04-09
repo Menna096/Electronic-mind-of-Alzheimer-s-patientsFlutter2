b1c5139a72a1d0f26b67f73ee5f74e3be1ba8155
Menna <mennaragab220@gmail.com>
2024-04-09 08:48:15 +0200

    caregiverId and family media fix

--- vv/lib/Caregiver/mainpagecaregiver/caregiver_id.dart
+++ vv/lib/Caregiver/mainpagecaregiver/caregiver_id.dart
@@ -174,3 +174,167 @@
 //     );
 //   }
 // }
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:vv/Caregiver/mainpagecaregiver/mainpagecaregiver.dart';
+import 'package:vv/api/login_api.dart';
+
+class caregiverCode extends StatefulWidget {
+  const caregiverCode({super.key});
+
+  @override
+  State<caregiverCode> createState() => _caregiverCodeState();
+}
+
+class _caregiverCodeState extends State<caregiverCode> {
+  String caregiverCode = 'Loading...';
+  @override
+  void initState() {
+    super.initState();
+    fetchAndSetCaregiverCode();
+  }
+
+  Future<void> fetchAndSetCaregiverCode() async {
+    try {
+      final response = await DioService().dio.get(
+          'https://electronicmindofalzheimerpatients.azurewebsites.net/Caregiver/GetCaregiverCode');
+      if (response.statusCode == 200) {
+        setState(() {
+          caregiverCode = response
+              .data; // Assuming the response data is the code as a String
+        });
+      } else {
+        setState(() {
+          caregiverCode =
+              'Error loading ID'; // Set an error message or handle differently
+        });
+      }
+    } catch (e) {
+      setState(() {
+        caregiverCode =
+            'Error: $e'; // Set an error message with exception details
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    if (caregiverCode == 'Loading...') {
+      return Scaffold(
+        backgroundColor: Colors.grey.shade300,
+        body: Center(child: CircularProgressIndicator()),
+      );
+    }
+
+    // Once patientCode is fetched, or if an error occurs (and is handled), the below UI is built
+    return Scaffold(
+      backgroundColor: Colors.grey.shade300,
+      body: Center(
+        child: Container(
+          width: double.infinity,
+          height: MediaQuery.of(context).size.height,
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              begin: Alignment.topCenter,
+              end: Alignment.bottomCenter,
+              colors: [Colors.transparent, Colors.blue.shade900],
+            ),
+          ),
+          child: Align(
+            alignment: Alignment.center,
+            child: Container(
+              width: 300,
+              padding: EdgeInsets.all(16.0),
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(12.0),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.black26,
+                    blurRadius: 8.0,
+                    offset: Offset(0, 2),
+                  ),
+                ],
+              ),
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: <Widget>[
+                  Text(
+                    'Your ID',
+                    style: TextStyle(
+                      fontSize: 24,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  SizedBox(height: 24),
+                  TextField(
+                    decoration: InputDecoration(
+                      labelText: 'Caregiver ID',
+                      border: OutlineInputBorder(
+                        borderRadius:
+                            BorderRadius.circular(8.0), // Rounded corners
+                        borderSide: BorderSide(
+                          color: Colors.black, // Default border color
+                          width: 1.0, // Default border width
+                        ),
+                      ),
+                      contentPadding: EdgeInsets.symmetric(
+                          vertical: 20.0, horizontal: 10.0),
+                      suffixIcon: IconButton(
+                        icon: Icon(Icons.copy),
+                        onPressed: () {
+                          Clipboard.setData(ClipboardData(text: caregiverCode))
+                              .then((_) {
+                            ScaffoldMessenger.of(context).showSnackBar(
+                              SnackBar(
+                                content: Text('Your ID copied to clipboard!'),
+                                duration: Duration(seconds: 2),
+                              ),
+                            );
+                          });
+                        },
+                      ),
+                    ),
+                    readOnly: true,
+                    controller: TextEditingController(text: caregiverCode),
+                    style: TextStyle(fontSize: 20),
+                    maxLines: null,
+                    keyboardType: TextInputType.multiline,
+                  ),
+                  SizedBox(height: 24),
+                  Text(
+                    'Please send it to the family',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(color: Colors.grey.shade600),
+                  ),
+                  SizedBox(height: 24),
+                  ElevatedButton(
+                    onPressed: () {
+                      Navigator.pushReplacement(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) =>
+                                mainpagecaregiver()), // Ensure this is the correct class name for your Main Page Family
+                      );
+                    },
+                    child: Text(
+                      'Done',
+                      style: TextStyle(color: Colors.white),
+                    ),
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.blue,
+                      fixedSize: Size(150, 50),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(30.0),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
--- vv/lib/page/gallery_screen.dart
+++ vv/lib/page/gallery_screen.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:dio/dio.dart';
+import 'package:image_picker/image_picker.dart';
 import 'package:vv/api/login_api.dart';
 import 'package:vv/models/media_item.dart';
+import 'package:vv/page/description_screen.dart';
 import 'package:vv/page/full_screen_viewer.dart';
 import 'package:vv/widgets/background.dart';
 
@@ -17,6 +19,91 @@ class _GalleryScreenState extends State<GalleryScreen> {
   void initState() {
     super.initState();
     fetchMedia(); // Fetch media when the screen initializes
+  }
+
+  void _addMedia() async {
+    final ImagePicker picker = ImagePicker();
+    final source = await showDialog<ImageSource>(
+      context: context,
+      builder: (BuildContext context) => Dialog(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            ListTile(
+              leading: Icon(Icons.camera),
+              title: Text('Camera'),
+              onTap: () => Navigator.pop(context, ImageSource.camera),
+            ),
+            ListTile(
+              leading: Icon(Icons.photo_library),
+              title: Text('Gallery'),
+              onTap: () => Navigator.pop(context, ImageSource.gallery),
+            ),
+          ],
+        ),
+      ),
+    );
+
+    if (source != null) {
+      final isVideo = await showDialog<bool>(
+            context: context,
+            builder: (BuildContext context) => AlertDialog(
+              title: const Text('Choose Media Type'),
+              content: const Text('What do you want to take?'),
+              actions: <Widget>[
+                TextButton(
+                  onPressed: () => Navigator.pop(context, false),
+                  child: const Text('Photo'),
+                ),
+                TextButton(
+                  onPressed: () => Navigator.pop(context, true),
+                  child: const Text('Video'),
+                ),
+              ],
+            ),
+          ) ??
+          false;
+
+      XFile? pickedFile;
+      MediaType type;
+
+      if (isVideo) {
+        // User chose to pick a video
+        pickedFile = await picker.pickVideo(
+          source: source,
+          maxDuration: const Duration(seconds: 60),
+        );
+        type = MediaType.video;
+      } else {
+        // User chose to pick an image
+        pickedFile = await picker.pickImage(
+          source: source,
+          imageQuality: 85,
+          maxWidth: 1920,
+          maxHeight: 1080,
+        );
+        type = MediaType.image;
+      }
+
+      if (pickedFile != null) {
+        // Navigate to the DescriptionScreen and wait for the result
+        final MediaItem? newMediaItem = await Navigator.of(context).push(
+          MaterialPageRoute(
+            builder: (context) => DescriptionScreen(
+              mediaPath: pickedFile!.path,
+              type: type,
+            ),
+          ),
+        );
+
+        // If a new media item was added, update the list and UI
+        if (newMediaItem != null) {
+          setState(() {
+            mediaItems.add(newMediaItem);
+          });
+        }
+      }
+    }
   }
 
   void fetchMedia() async {
@@ -67,29 +154,34 @@ class _GalleryScreenState extends State<GalleryScreen> {
           itemBuilder: (context, index) {
             var mediaItem = mediaItems[index];
             return Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: InkWell(
-                onTap: () => _viewMediaItem(mediaItem),
-                child: ClipRRect(
-                  borderRadius: BorderRadius.circular(16.0),
-                  child: mediaItem.type == MediaType.image
-                      ? Image.network(mediaItem.path, fit: BoxFit.cover)
-                      : Stack(
-                          alignment: Alignment.center,
-                          children: [
-                            Positioned.fill(
-                              child: Image.network(mediaItem.path,
-                                  fit: BoxFit.cover),
+                padding: const EdgeInsets.all(8.0),
+                child: InkWell(
+                  onTap: () => _viewMediaItem(mediaItem),
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(16.0),
+                    child: mediaItem.type == MediaType.image
+                        ? Image.network(mediaItem.path, fit: BoxFit.cover)
+                        : DecoratedBox(
+                            decoration: BoxDecoration(
+                              image: DecorationImage(
+                                image: AssetImage(
+                                    'images/vid.png'), // Use the placeholder image for videos
+                                fit: BoxFit.cover,
+                              ),
                             ),
-                            Icon(Icons.play_circle_outline,
-                                size: 50, color: Colors.white),
-                          ],
-                        ),
-                ),
-              ),
-            );
+                            child: Center(
+                              child: Icon(Icons.play_circle_outline,
+                                  size: 50, color: Colors.white),
+                            ),
+                          ),
+                  ),
+                ));
           },
         ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _addMedia,
+        child: Icon(Icons.add),
       ),
     );
   }
