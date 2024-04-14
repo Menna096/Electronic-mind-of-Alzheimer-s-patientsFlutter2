e929afe110441e8d02da4b2e15d311c2a447d32b
Menna <moui48973@gmail.com>
2024-04-14 05:49:31 +0200

    DONE
    MEDICAL CAREGIVER

--- vv/macos/Flutter/GeneratedPluginRegistrant.swift
+++ vv/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -14,6 +14,7 @@ import geolocator_apple
 import just_audio
 import path_provider_foundation
 import record_darwin
+import shared_preferences_foundation
 import sqflite
 import url_launcher_macos
 import video_player_avfoundation
@@ -28,6 +29,7 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   JustAudioPlugin.register(with: registry.registrar(forPlugin: "JustAudioPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   RecordPlugin.register(with: registry.registrar(forPlugin: "RecordPlugin"))
+  SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
   SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
   UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
   FVPVideoPlayerPlugin.register(with: registry.registrar(forPlugin: "FVPVideoPlayerPlugin"))
