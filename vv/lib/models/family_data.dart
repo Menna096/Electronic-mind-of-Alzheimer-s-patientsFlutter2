0bb819e7434fccd6921521f83385edccedcacd05
Menna <moui48973@gmail.com>
2024-04-28 05:48:22 +0300

    done
    description of family to patient in gps

--- vv/lib/models/family_data.dart
+++ vv/lib/models/family_data.dart
@@ -2,19 +2,23 @@ class FamilyMember {
   String familyId;
   String familyName;
   String relationility;
+  String? familyDescriptionForPatient; // Changed to nullable type
   String hisImageUrl;
 
-  FamilyMember(
-      {required this.familyId,
-      required this.familyName,
-      required this.relationility,
-      required this.hisImageUrl});
+  FamilyMember({
+    required this.familyId,
+    required this.familyName,
+    required this.relationility,
+    this.familyDescriptionForPatient, // Removed required keyword
+    required this.hisImageUrl,
+  });
 
   factory FamilyMember.fromJson(Map<String, dynamic> json) {
     return FamilyMember(
-      familyId: json['familyId'], // Added familyId field here
+      familyId: json['familyId'], 
       familyName: json['familyName'],
       relationility: json['relationility'],
+      familyDescriptionForPatient: json['familyDescriptionForPatient'],
       hisImageUrl: json['hisImageUrl'],
     );
   }
