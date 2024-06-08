5ac42dfa4592d2b261b723c72583ebb2fb78b80c
Menna <moui48973@gmail.com>
2024-06-09 01:00:02 +0300

    done

--- vv/lib/Caregiver/medical/models/medicine_type.dart
+++ vv/lib/Caregiver/medical/models/medicine_type.dart
@@ -1,2 +1,9 @@
 // ignore: constant_identifier_names
-enum MedicineType { Pill, Syringe, Bottle, Tablet, None }
+enum MedicineType {
+    Pill,
+    Syringe,
+    Bottle, 
+    Tablet,
+    None 
+   
+   }
