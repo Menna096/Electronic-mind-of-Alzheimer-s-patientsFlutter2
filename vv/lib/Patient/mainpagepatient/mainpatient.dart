3fbe310b575810fd8d5d75f573e3973fbbf7ff6f
typ-AhmedSleem <theyoungprogrammer67@gmail.com>
2024-06-25 09:56:42 +0300

    chatbot: chatbot is now fully integrated within the project

--- vv/lib/Patient/mainpagepatient/mainpatient.dart
+++ vv/lib/Patient/mainpagepatient/mainpatient.dart
@@ -3,6 +3,7 @@ import 'dart:convert';
 import 'dart:math';
 
 import 'package:chat_bot/chat/ui/chat_screen.dart';
+import 'package:chat_bot/chatbot_secrets.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:flutter_timezone/flutter_timezone.dart';
@@ -686,7 +687,7 @@ class _mainpatientState extends State<mainpatient> {
                             onTap: () {
                               Navigator.push(
                                 context,
-                                MaterialPageRoute(builder: (context) => const ChatScreen()),
+                                MaterialPageRoute(builder: (context) => ChatScreen(token: authToken)), // todo: replace authToken with your token
                               );
                             },
                             child: Image.asset(
